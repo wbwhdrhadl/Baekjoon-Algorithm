8964d51352c1f8bae91515b1015a904c565b23c5
LEE DA EUN <ldaeun.dev@gmail.com>
2025-10-16 13:02:44 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 608 ms, Memory: 60904 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n,m,r = map(int,input().split())
+
+graph = [[] for _ in range(n+1)]
+
+for i in range(m):
+    u,v = map(int,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(1,n+1):
+    graph[i].sort(reverse=True)
+visited = [0] * (n+1)
+order = 1
+
+def bfs(start):
+    global order
+    queue = deque([start])
+    visited[start] = order
+    
+    while queue:
+        node = queue.popleft()
+        for nxt in graph[node]:
+            if visited[nxt] == 0:
+                order += 1
+                visited[nxt] = order
+                queue.append(nxt)
+                
+                
+bfs(r)
+
+for i in range(1,n+1):
+    print(visited[i])
